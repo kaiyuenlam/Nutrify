7dd6adc8c3aa8708d36d98fb003ec4350e3c68c3
micchan7 <michellechan0077@gmail.com>
2024-11-27 18:27:05 +0800

    Upload NutritionDataModel

---
+++ NutritionDataModel.swift
@@ -0,0 +1,18 @@
+//
+//  NutritionDataModel.swift
+//  Nutrify
+//
+//  Created by Michelle Chan on 26/11/2024.
+//
+import Foundation
+import Combine
+
+class NutritionDataModel: ObservableObject {
+    @Published var calorieGoal: Int = 2700
+    @Published var calorieIntake: Int = 0
+    @Published var exerciseCalories: Int = 500
+    @Published var fatPercentage: Double = 0
+    @Published var proteinPercentage: Double = 0
+    @Published var carbPercentage: Double = 0
+    @Published var weightToLose: Double = 0
+}
